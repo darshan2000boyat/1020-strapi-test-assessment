971ce5b37472bb214e6c29314297612b1d4668bc
Darshan Boyat <darshan@tentwenty.me>
2025-12-05 17:11:38 +0530

    Migration to cloud

---
+++ src/api/timesheet/controllers/timesheet.ts
@@ -0,0 +1,7 @@
+/**
+ * timesheet controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::timesheet.timesheet');
---
+++ src/api/timesheet/services/timesheet.ts
@@ -0,0 +1,7 @@
+/**
+ * timesheet service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::timesheet.timesheet');
